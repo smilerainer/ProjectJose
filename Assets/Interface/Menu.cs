8aabc3ea6c0a1baf4bebaf27add4541fb44900a9
spoonlimp <125034871+spoonlimp@users.noreply.github.com>
2025-08-22 22:59:41 +0800

    m,

--- Assets/Interface/Menu.cs
+++ Assets/Interface/Menu.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Menu : Node2D
+public partial class Menu : CanvasLayer
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
